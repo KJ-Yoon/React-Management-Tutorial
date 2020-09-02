92949d68f098f391fc97bc94cc20534422f3bfc6
zamta <zamta6344@gmail.com>
2020-09-02 17:43:50 +0900

    chapter 5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App" className="gray-background">
-      <img src={logo} lat="logo"></img>
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '윤경재',
+    'birthday': '791221',
+    'gender': '남자',
+    'job': 'IRONMAN'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '이순신',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '고길동',
+    'birthday': '921202',
+    'gender': '남자',
+    'job': '직장인'
+  }
+]
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        {
+          customers.map(c =>{
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
